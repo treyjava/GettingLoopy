d8b9752ba9d15e21b895cb6ff6fbc9523d3948ab
Trey <burlints@mail.uc.edu>
2022-10-06 14:37:29 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        for(int cnt = 0; cnt <= 30; cnt ++)
+        {
+            System.out.println(cnt);
+        }
+
+
+        for(int cnt = 30; cnt >= 0; cnt --)
+        {
+            System.out.println(cnt);
+        }
+
+
+        for(int cnt = 0; cnt <= 18; cnt = cnt + 3)
+        {
+            System.out.println(cnt);
+        }
+
+
+        for(int cnt = 10; cnt >= 0; cnt = cnt - 2)
+        {
+            System.out.println(cnt);
+        }
+    }
+}
